c878242bcb4912be0803b79c51f94b29ed69f34b
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-12-21 17:32:36 -0300

    project ecommerce

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '053ef3ff04f21a3227ad862bee57a832'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '932fd4cd-515e-4a6d-98f0-073e97959a4b'
+};
